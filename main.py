ac8509f77b7d17b0f4688797a7fec3cbd3476e83
bdbrink <brendan.brink@outlook.com>
2023-04-05 17:45:22 -0500

    adding simple model func

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import gym
 
 env = gym.make("CartPole-v1", render_mode="human")
 
-episodes = 10
-for episode in range(1, episodes+1):
-    state = env.reset()
-    done = False
-    score = 0
+def simple_model():
+    episodes = 10
+    for episode in range(1, episodes+1):
+        state = env.reset()
+        done = False
+        score = 0
 
-    while not done:
-        action = random.choice([0, 1])
-        _, reward, done, _ = env.step(action)
-        score += reward
-        env.render()
+        while not done:
+            action = random.choice([0, 1])
+            _, reward, done, _ = env.step(action)
+            score += reward
+            env.render()
 
-    print(f"Episode {episode}, Score: {score}")
+        print(f"Episode {episode}, Score: {score}")
 
-env.close()
+    env.close()
